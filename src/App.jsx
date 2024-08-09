f03f1244cff6f4a13ecc173823bb6fb77812021c
Bárbara Ferreira <barbara_ferreira_3@hotmail.com>
2024-08-09 20:19:52 +0100

    Carousel working

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={< Home />} />
+      </Routes>
+
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+function Home() {
+  return (
+    <>
+        <Carousel data-bs-theme="dark">
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="https://d.newsweek.com/en/full/1898130/dog-cat-under-sheet.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h5>First slide label</h5>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="https://d.newsweek.com/en/full/1898130/dog-cat-under-sheet.jpg"
+                alt="Second slide"
+                />
+                <Carousel.Caption>
+                <h5>Second slide label</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="https://d.newsweek.com/en/full/1898130/dog-cat-under-sheet.jpg"
+                alt="Third slide"
+                />
+                <Carousel.Caption>
+                <h5>Third slide label</h5>
+                <p>
+                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+
+    </>
+  );
+}
+
+export default Home;
