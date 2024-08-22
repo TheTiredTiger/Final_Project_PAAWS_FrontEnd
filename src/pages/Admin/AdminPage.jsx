3c3872e7e61cbe94fbf9f67d4eb5d59d15ab924e
Bárbara Ferreira <barbara_ferreira_3@hotmail.com>
2024-08-22 23:14:46 +0100

    AdminPage routes all working, Footer is now at the bottom but still needs some tweaking, added Admin link to the Navbar

---
+++ src/pages/Admin/AdminPage.jsx
@@ -0,0 +1,62 @@
+// General Admin Page to more easily route towards adding, editing, deleting and handling adoption processes
+
+import Row  from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from 'react-bootstrap/Card';
+import Button  from "react-bootstrap/Button";
+
+function AdminPage() {
+    return ( <>
+        <Row >
+            <Col lg="6" >
+                <Card style={{ width: "18rem", height: "14rem"}}>
+                <Card.Body>
+                    <Card.Title>Add animal</Card.Title>
+                    <Card.Text>
+                    Form to add new animal to the API.
+                    </Card.Text>
+                    <Button href="/adminadd">Add</Button>
+                </Card.Body>
+                </Card>            
+            </Col>
+            <Col lg="6">
+                <Card style={{ width: "18rem", height: "14rem"}}>
+                <Card.Body>
+                    <Card.Title>Edit animal</Card.Title>
+                    <Card.Text>
+                    Form to edit existing animal.
+                    </Card.Text>
+                    <Button href="/adminedit">Edit</Button>
+                </Card.Body>
+                </Card>            
+            </Col>
+        </Row>
+        <Row >
+            <Col lg="6" >
+                <Card style={{ width: "18rem", height: "14rem"}}>
+                <Card.Body>
+                    <Card.Title>Delete animal</Card.Title>
+                    <Card.Text>
+                    Form to delete an animal from the API.
+                    </Card.Text>
+                    <Button href="/admindelete">Delete</Button>
+                </Card.Body>
+                </Card>            
+            </Col>
+            <Col lg="6">
+                <Card style={{ width: "18rem", height: "14rem"}}>
+                <Card.Body>
+                    <Card.Title>Edit adoption status</Card.Title>
+                    <Card.Text>
+                    Form to edit adoption status of an existing animal.
+                    </Card.Text>
+                    <Button href="/adoptionstatus">Edit</Button>
+                </Card.Body>
+                </Card>            
+            </Col>
+        </Row>
+    
+    </> );
+}
+
+export default AdminPage;
