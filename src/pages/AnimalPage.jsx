ca8386a42885b0dcfb90019d532ae126853bc048
Bárbara Ferreira <barbara_ferreira_3@hotmail.com>
2024-08-24 21:38:43 +0100

    AnimalPage still not working, updated routes for AdoptionForm

--- src/pages/AnimalPage.jsx
+++ src/pages/AnimalPage.jsx
@@ -2,32 +2,40 @@
 
 // Section below leading to adoption form
 
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { useAPI } from './Context/Context'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-function AnimalCard() {
+function AnimalPage({ animal }) {
+  const { getAnimal } = useAPI()
+  const { id } = useParams();
+
+  console.log("I am animal object in animal page", animal)
+
+
   return (
     <>
-      <Card className='animalPage'>
+      <Card className='animalPage' key={animal}>
         <Row>
           <Col lg="5" >
-            <Card.Img className="animalPageImg" variant="top" src="https://cdn.britannica.com/86/166986-050-4CEFE5DE/cute-kitten-and-puppy-outdoors-in-grass.jpg" alt="animal picture" />
+            <Card.Img className="animalPageImg" variant="top" src=""
+            alt="animal picture" />
           </Col>
           <Col lg="7" >
             <Card.Body className='animalPageDesc'>
-              <Card.Title>Name</Card.Title>
+              <Card.Title>{animal.name || "Unknown Animal"}</Card.Title>
               <Card.Text>
-                Species: <br />
-                Gender: <br />
-                Age: <br />
-                Weight: <br />
-                Breed: <br />
-                Location: <br />
-                Known illnesses: <br />
-                Description:
+                Species: {animal.species || "Unknown"} <br />
+                Gender: {animal.gender || "Unknown"} <br />
+                Age: {animal.life_stage || "Unknown"} <br />
+                Weight: {animal.weight || "Unknown"}<br />
+                Breed: {animal.breed || "Unknown"}<br />
+                Location: {animal.location || "Unknown"}<br />
+                Known illnesses: {animal.known_illness || "Unknown"}<br />
+                Description: {animal.description || "Unknown"}
               </Card.Text>
               <div className="animalPageBtn">
                 <Button variant="primary" style={{ marginRight: "1rem" }}>
@@ -48,10 +56,10 @@ function AnimalCard() {
 
       <div className="toAdoptionForm">
         <h4>Interested in adopting instead?</h4>
-        <p>Check out our <Link to="/adoptionForm" >form</Link></p>
+        <p>Check out our <Link to={`/adoptionform/${animal.id}`} >form</Link></p>
       </div>
     </>
   );
 }
 
-export default AnimalCard;
+export default AnimalPage;
