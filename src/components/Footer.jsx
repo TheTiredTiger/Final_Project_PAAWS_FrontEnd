9631b9da93acc3b7f1c910f190aa8f1ff291d6f5
Bárbara Ferreira <barbara_ferreira_3@hotmail.com>
2024-08-31 23:29:08 +0100

    Animal Page images are all the same height, changed Previous and Next buttons to chevrons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,13 +9,10 @@ function Footer() {
     <Navbar collapseOnSelect expand="lg" className="footer mt-5" >
       <Container fluid>
         {/* <Image src="src/images/PAAWS footer image.png" alt='mountain range with trees at the foreground' className='footerImg' fluid />; */}
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto footerItem ps-2">
             <Nav.Link>© All pawprints reserved by Alexis Mendes and Bárbara Ferreira</Nav.Link>
             {/* <Nav.Link href="/FAQ">FAQ</Nav.Link> */}
           </Nav>
-        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
